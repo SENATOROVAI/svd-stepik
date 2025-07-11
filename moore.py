3ab558ae8c2b95192b4281b69b77fb3923d068b9
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2025-07-12 02:23:57 +0300

    Update moore.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- moore.py
+++ moore.py
@@ -1,5 +1,5 @@
 arr = np.array([[2, 4], [1, 3], [0, 0], [0, 0]])
-U, S, VT = np.linalg.svd(arr)
+U, S, VT = 
 Sigma = np.diag(S)
 Sigma_inverse = np.linalg.ОБРАТНАЯ_МАТРИЦА(Sigma)
 Sigma_plus = np.concatenate((Sigma_inverse, np.array([[0, 0], [0, 0]]).T), axis=1)
